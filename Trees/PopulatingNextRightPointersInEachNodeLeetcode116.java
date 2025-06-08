99ad25d0ca8321b50099475ef9f948e9ce1cd64b
Harsh Srivastav <hsrivastav496@gmail.com>
2025-06-08 17:01:22 +0530

    Update PopulatingNextRightPointersInEachNodeLeetcode116.java

--- Trees/PopulatingNextRightPointersInEachNodeLeetcode116.java
+++ Trees/PopulatingNextRightPointersInEachNodeLeetcode116.java
@@ -1,8 +1,8 @@
-//            1                         1 -> null
-//          /   \                     /   \
-//         2     3      ------>      2 --> 3 -> null
-//        / \   / \                 / \   / \
-//       4   5 6   7               4-> 5->6->7-> null
+//        1                 1 -> null
+//      /   \             /   \
+//     2     3  ----->   2 --> 3 -> null
+//    / \   / \         / \   / \
+//   4   5 6   7       4-> 5->6->7-> null
 
 // To solve this problem we will use the approach -> Assume the root node as leftmost node initially and connect its left and right child and then traverse right in the same level , if it's next is not null . Note that on moving to right in same level you should connect the previous right child to new node left child (here on moving to 3 from 2 we connect 5 to 6 ).If next node is null then we will traverse to the next level down(here in case of 1 no next node , so we move down to 2nd level having 2 and 3) and repeat the process until we reach the node that has no leftchild(here 4) no need to check beyond beacuse this is perfect binary tree.  
 
