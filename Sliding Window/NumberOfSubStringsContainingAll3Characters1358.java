ae75e9ddaaaeb803e88ebd929017bee11c136c96
Harsh Srivastav <hsrivastav496@gmail.com>
2025-10-06 22:15:53 +0530

    Update NumberOfSubStringsContainingAll3Characters1358.java

--- Sliding Window/NumberOfSubStringsContainingAll3Characters1358.java
+++ Sliding Window/NumberOfSubStringsContainingAll3Characters1358.java
@@ -39,3 +39,33 @@ public class NumberOfSubStringsContainingAll3Characters1358 {
 }
 
 }
+
+
+// ALERNATE SOLUTION
+
+class Solution {
+    public int numberOfSubstrings(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l = 0;
+        int r = 0;
+        int count = 0;
+        while (r < s.length()) {
+
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0) + 1);
+
+            while (map.size() == 3) {
+                count += s.length()-1-(r+1)+1 +1 ;
+                map.put(s.charAt(l), map.get(s.charAt(l)) - 1);
+                if (map.get(s.charAt(l)) == 0) {
+                    map.remove(s.charAt(l));
+                }
+                l++;
+            }
+
+            r++;
+
+        }
+        return count;
+
+    }
+}
